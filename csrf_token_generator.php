5c7975705c304771a5df0341656b5d0a202c0913
Nimeshika Ranasinghe <nimeshika.ishani@gmail.com>
2018-05-24 00:45:11 +0530

    Add files via upload

--- csrf_token_generator.php
+++ csrf_token_generator.php
@@ -1,17 +1,18 @@
 <?php
 
-session_start();
-$myfile = fopen("CSFR_Tokens.txt", "w") or die("Unable to open file!");
-$_SESSION['token'] = base64_encode(openssl_random_pseudo_bytes(32));
-$txt = $_SESSION['token'].",";
+	session_start();
+	$myfile = fopen("CSFR_Tokens.txt", "w") or die("Unable to open file!");
+	$_SESSION['csrf_token'] = base64_encode(openssl_random_pseudo_bytes(32));
+	$txt = $_SESSION['csrf_token'].",";
+	fwrite($myfile, $txt);
 
-fwrite($myfile, $txt);
-$session_id = session_id();
-setcookie('CSFR1',$session_id,time()+60*60*24*365,'/');
-$txt1 = $session_id."\n";
-fwrite($myfile, $txt1);
-fclose($myfile);
+	$session_id = session_id();
+	setcookie('cookieName_sessionID',$session_id,time()+60*60*24*365,'/');
+	$txt1 = $session_id."\n";
+	fwrite($myfile, $txt1);
 
-echo $_SESSION['token'];
+	fclose($myfile);
+
+	echo $_SESSION['csrf_token'];
 
 ?>
--- index.php
+++ index.php
@@ -2,21 +2,21 @@
 <html>
 	<head>
 		<title>Cross Site Request Forgery Protection</title>
+        <link rel="stylesheet" type="text/css" href="all_styles.css">   
 	</head>
-	<body>
-		<form action="result.php" method="post">
-			<div class="login">
-				<h1>Login Form</h1>
-					<div class="credentials">
-							Username: <input type="text" name="username"> 
-                            <br> <br>
-							Password: <input type="text" name="password">
-					</div>
-                    <br>
-					<input type="Submit" value="Login">
-					
-			</div>
-		</form>
+    
+	<body>        
+        <div id="parent">
+            <form action="home.php" method="post" id="form_login">                
+                <h1>Login Form</h1>
+                <div id="credentials">
+                    Username: <input type="text" name="username" placeholder="Enter Username">                    <br> <br>
+                    Password: <input type="password" name="password" placeholder="Enter Username">
+                    <br> <br>
+                	<input type="Submit" value="Login">  
+                </div>                    
+            </form>
+        </div>
 	</body> 
 </html>
 
